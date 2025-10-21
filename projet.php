7e498bbefdf8f131a77bfe25c97244a21708d80c
Samuel César <e2044458@cmaisonneuve.qc.ca>
2025-10-21 17:02:07 -0400

    Create projet.php

---
+++ projet.php
@@ -0,0 +1,25 @@
+<section class="titrePageProjet">
+    <div class="crochets">
+        <div class="boiteTitrePageProjet">            
+            <div class="titrePageProjet"></div>
+            <div class="etudiants">
+                <div class="etudiant">
+                    <span>Malaika Abevi</span>
+                </div>
+                <div class="etudiant">
+                    <span>Sebastien Malo</span>
+                </div>
+                <div class="etudiant">
+                    <span>Yanis Oulmane</span>
+                </div>
+                <div class="etudiant">
+                    <span>Matys Voisin</span>
+                </div>
+            </div>
+        </div>
+    </div>    
+</section>
+
+<section class="informationPageProjet">
+
+</section>

9300ebdf2f8a7d9c368b4c6d8a84a492a9246ce3
mazpen <Azpensbri@gmail.com>
2025-10-23 15:13:15 -0400

    s1: debug scss a propos

--- template-apropos.php
+++ template-apropos.php
@@ -1,38 +1,42 @@
 <?php
-/*
-Template Name: À Propos
-*/
-//a-propos
+   /*
+   Template Name: À Propos
+   */
+   //a-propos
 ?>
-<?php get_header(); ?>
 
+   <?php get_header(); ?>
+   
+   <main>
       <!-- Titre de la page -->
-      <section class="titre">
+      <section class="titre__aPropos">
          <h2>À Propos</h2>
       </section>
 
       <!-- Notre but -->
       <section class="infosaPropos">
          <h3> Notre But</h3>
-         <p>
-            Notre équipe est composée de cinq étudiants en Technique d'intégration multimédia au Collège de
-            Maisonneuve. Nous avons créé ce site web dans le cadre d'un projet scolaire visant à promouvoir 
-            l'exposition de fin d'année. Notre objectif est de fournir une plateforme en ligne pour présenter
-            et archiver les projets des étudiants fait à travers les années.
-         </p>
+         <div>
+            <p>
+               Notre équipe est composée de cinq étudiants en Technique d'intégration multimédia au Collège de
+               Maisonneuve. Nous avons créé ce site web dans le cadre d'un projet scolaire visant à promouvoir 
+               l'exposition de fin d'année. Notre objectif est de fournir une plateforme en ligne pour présenter
+               et archiver les projets des étudiants fait à travers les années.
+            </p>
+         </div>
+         
       </section>
 
       <section class="profsPivots">
          <h3> Les profs pivots</h3>
          <p>David Ross bureau: F-3004  dross@cmaisonneuve.qc.ca </p>
          <p>Grégory Bony bureau: F-3004  gbony@cmaisonneuve.qc.ca </p>
-
       </section>
 
 
 
       <!-- Liens utiles -->
-      <section class="liens_aPropos">
+      <section class="liens__aPropos">
          <h3>Liens utiles</h3>
          <ul>
             <a href="https://www.cmaisonneuve.qc.ca/programme/integration-multimedia/">Technique d'intégration Multimédia</a>
@@ -40,8 +44,10 @@ Template Name: À Propos
          </ul>         
       </section>
 
-      <?php get_footer(); ?>
-
+   </main>
+   <footer>
+     <?php get_footer(); ?>
+   </footer>
 </body>
 
 </html>

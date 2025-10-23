9d35eb9042b2c6ddf07645b481e25e4d6016651c
Samuel César <e2044458@cmaisonneuve.qc.ca>
2025-10-23 17:45:47 -0400

    début page projet

--- projet.php
+++ projet.php
@@ -1,18 +1,20 @@
-<section class="titrePageProjet">
+<section class="pageProjet__titre">
     <div class="crochets">
-        <div class="boiteTitrePageProjet">            
-            <div class="titrePageProjet"></div>
-            <div class="etudiants">
-                <div class="etudiant">
+        <div class="pageProjet__titre__boiteTitre">            
+            <div class="pageProjet__titre__boiteTitre__texte">
+                <h2>Hachiman</h2>
+            </div>
+            <div class="pageProjet__titre__boiteTitre_etudiants">
+                <div class="pageProjet__titre__boiteTitre__etudiants__contenant">
                     <span>Malaika Abevi</span>
                 </div>
-                <div class="etudiant">
+                <div class="pageProjet__titre__boiteTitre__etudiants__contenant">
                     <span>Sebastien Malo</span>
                 </div>
-                <div class="etudiant">
+                <div class="pageProjet__titre__boiteTitre__etudiants__contenant">
                     <span>Yanis Oulmane</span>
                 </div>
-                <div class="etudiant">
+                <div class="pageProjet__titre__boiteTitre__etudiants__contenant">
                     <span>Matys Voisin</span>
                 </div>
             </div>
@@ -20,6 +22,40 @@
     </div>    
 </section>
 
-<section class="informationPageProjet">
+<section class="pageProjet__contenu">
+    <div class="pageProjet__contenu__affiche">
+        <img src="hachiman.jpg" alt="">
+    </div>
+    <div class="pageProjet__contenu__information">
+        <div class="pageProjet__contenu__information__description">
+            <h4>Description</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dolor quibusdam quam nulla itaque. Non, adipisci sunt. Impedit explicabo, ratione unde distinctio delectus voluptatum fuga nobis rem quod nesciunt! Ipsum?</p>
+        </div>
+        <div class="pageProjet__contenu__information__video">
+            <video src="hachiman.mp4"></video>
+            <div class="pageProjet__contenu__information__video__boiteFond">
+                <div class="pageProjet__contenu__information__video__boiteFond__boiteTexte">
+                    <p>Description de la vidéo</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
+<section class="pageProjet__carrousel">
+    <div class="pageProjet__carrousel__images">
+        <button class="pageProjet__carrousel__images__fleche"></button>
+        <img src="hachiman1.jpg" alt="">
+        <img src="hachiman2.jpg" alt="">
+        <img src="hachiman3.jpg" alt="">
+        <img src="hachiman4.jpg" alt="">
+        <img src="hachiman5.jpg" alt="">
+        <button class="pageProjet__carrousel__images__fleche"></button>
+    </div>
+    <div class="pageProjet__carrousel__boutons">
+        <input type="radio">
+        <input type="radio">
+        <input type="radio">
+        <input type="radio">
+    </div>
 </section>

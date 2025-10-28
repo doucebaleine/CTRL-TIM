383c52a248904e0ab21427e2b486b58abe2016f0
Weiqiang Chen <150718801+Kensho17@users.noreply.github.com>
2025-10-28 17:30:41 -0400

    avancement page accueil sur les cartes et titre

--- index.php
+++ index.php
@@ -6,15 +6,14 @@
          <h1>CTRL+TIM</h1>
          <img class="effetTitre" src="<?php echo esc_url( get_template_directory_uri() . '/images/effetTitre.svg' ); ?>" aria-hidden="true" />
          <img class="effetTitre2" src="<?php echo esc_url( get_template_directory_uri() . '/images/effetTitre.svg' ); ?>" aria-hidden="true" />
-      </section>
-
-      <!-- DESCRIPTION DE l'EXPOSITION -->
-      <section class="descriptionExpoPrincipale">
+         <div class="descriptionExpoPrincipale">
          <h2>EXPOSITION MULTIMÉDIA</h2>
          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis harum laboriosam optio ipsam aut eveniet
             at neque error suscipit minima nostrum, necessitatibus rem. Cum sequi praesentium, blanditiis quam corrupti
             eveniet?</p>
+         </div>
       </section>
+
          <!-- Galerie de posters (empilés) placée à droite du main -->
          <div class="galerieBoiteGenerale" aria-hidden="false">
             <div class="poster-stack" aria-live="polite">

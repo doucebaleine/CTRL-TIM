8a47ddceec1fd43e8dfde3ddebd957cc28e4cf0a
mazpen <Azpensbri@gmail.com>
2025-11-06 14:36:55 -0500

    s2: nomanclature sass

--- template-apropos.php
+++ template-apropos.php
@@ -7,38 +7,20 @@
 
    <?php get_header(); ?>
    
-   <main>
-      <!-- Titre de la page -->
-      <section class="titreApropos">
-         <h2>À Propos</h2>
-      </section>
-
-      <!-- Notre but -->
-      <section class="infosaPropos">
-         <h3> Notre But</h3>
-         <div class="infosaPropos__textBut">
-            <p>
-               Notre équipe est composée de cinq étudiants en Technique d'intégration multimédia au Collège de
-               Maisonneuve. Nous avons créé ce site web dans le cadre d'un projet scolaire visant à promouvoir 
-               l'exposition de fin d'année. Notre objectif est de fournir une plateforme en ligne pour présenter
-               et archiver les projets des étudiants fait à travers les années.
-            </p>
-         </div>
-         
-      </section>
+   <main class="mainArtistes">
 
       <!-- les eleves -->
-      <section class="artistesAPropos">
-         <h3> Les artiste</h3>
+      <section class="artistes">
+         <h2> Les artistes</h2>
          <!-- boite artiste -->
-         <div class="artistesAPropos__artistes">
+         <div class="artistes__boiteArtistes">
 
             <!-- artiste 1 -->
-            <div class="artistesAPropos__artistes__artiste">
+            <div class="artistes__boiteArtistes__artisteSingle">
                <!-- Image artiste 1 -->
-               <img src="../images/GregoryBony.png" alt="Méduse Mongeau" class="artistesAPropos__artistes__artiste__imgArtiste">
+               <img src="../images/GregoryBony.png" alt="Méduse Mongeau" class="artistes__boiteArtistes__artisteSingle__imgArtiste">
                <!-- Info artiste 1 -->
-               <div class="artistesAPropos__artistes__artiste__infoArtiste">
+               <div class="artistes__boiteArtistes__artisteSingle__infoArtiste">
                   <p>Méduse Mongeau</p>
                   <p>fact?</p>
                   <p>link1</p>
@@ -46,11 +28,11 @@
             </div>
             
             <!-- artiste 2 -->
-            <div class="artistesAPropos__artistes__artiste">
+            <div class="artistes__boiteArtistes__artisteSingle">
                <!-- Image artiste 2 -->
-               <img src="../images/GregoryBony.png" alt="Samuel César" class="artistesAPropos__artistes__artiste__imgArtiste">
+               <img src="../images/GregoryBony.png" alt="Samuel César" class="artistes__boiteArtistes__artisteSingle__imgArtiste">
                <!-- Info artiste 2 -->
-               <div class="artistesAPropos__artistes__artiste__infoArtiste">
+               <div class="artistes__boiteArtistes__artisteSingle__infoArtiste">
                   <p>Samuel César</p>
                   <p>fact?</p>
                   <p>link1</p>
@@ -58,11 +40,11 @@
             </div>
 
             <!-- artiste 3 -->
-            <div class="artistesAPropos__artistes__artiste">
+            <div class="artistes__boiteArtistes__artisteSingle">
                <!-- Image artiste 3 -->
-               <img src="../images/GregoryBony.png" alt="Sébastien Malo" class="artistesAPropos__artistes__artiste__imgArtiste">
+               <img src="../images/GregoryBony.png" alt="Sébastien Malo" class="artistes__boiteArtistes__artisteSingle__imgArtiste">
                <!-- Info artiste 3 -->
-               <div class="artistesAPropos__artistes__artiste__infoArtiste">
+               <div class="artistes__boiteArtistes__artisteSingle__infoArtiste">
                   <p>Sébastien Malo</p>
                   <p>fact?</p>
                   <p>link1</p>
@@ -70,11 +52,11 @@
             </div>
             
             <!-- artiste 4 -->
-            <div class="artistesAPropos__artistes__artiste">
+            <div class="artistes__boiteArtistes__artisteSingle">
                <!-- Image artiste 4 -->
-               <img src="../images/GregoryBony.png" alt="Weiqiang Chen" class="artistesAPropos__artistes__artiste__imgArtiste">
+               <img src="../images/GregoryBony.png" alt="Weiqiang Chen" class="artistes__boiteArtistes__artisteSingle__imgArtiste">
                <!-- Info artiste 4 -->
-               <div class="artistesAPropos__artistes__artiste__infoArtiste">
+               <div class="artistes__boiteArtistes__artisteSingle__infoArtiste">
                   <p>Weiqiang Chen</p>
                   <p>fact?</p>
                   <p>link1</p>
@@ -83,11 +65,11 @@
             </div>
             
             <!-- artiste 5 -->
-            <div class="artistesAPropos__artistes__artiste">
+            <div class="artistes__boiteArtistes__artisteSingle">
                <!-- Image artiste 5 -->
-               <img src="../images/GregoryBony.png" alt="Azpen Sbrizzi" class="artistesAPropos__artistes__artiste__imgArtiste">
+               <img src="../images/GregoryBony.png" alt="Azpen Sbrizzi" class="artistes__boiteArtistes__artisteSingle__imgArtiste">
                <!-- Info artiste 5 -->
-               <div class="artistesAPropos__artistes__artiste__infoArtiste">
+               <div class="artistes__boiteArtistes__artisteSingle__infoArtiste">
                   <p>Azpen Sbrizzi</p>
                   <p>fact?</p>
                   <p>link1</p>
